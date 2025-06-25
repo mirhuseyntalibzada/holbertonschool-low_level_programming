55a5d12f52e43fc28472df16153ab604762fd404
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-06-25 11:27:15 +0400

    atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,31 @@
 #include "main.h"
 
-/**
- * _atoi - Converts a string to an integer
- * @s: The string to convert
- *
- * Return: The converted integer, or 0 if no digits found
- */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int started = 0;
+	int number, sign, index, inside_of_digit;
 
-	while (s[i] != '\0')
+	index = 0;
+	number = 0;
+	sign = 1;
+	inside_of_digit = 0;
+	
+	while (s[index] != '\0')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
+		if (s[index] == '-')
 		{
-			started = 1;
-			if (result > (2147483647 - (s[i] - '0')) / 10)
-			{
-				if (sign == 1)
-					return (2147483647);
-				else
-					return (-2147483648);
-			}
-			result = result * 10 + (s[i] - '0');
+			sign *= -1;
 		}
-		else if (started)
+		else if (s[index] >= '0' && s[index] <= '9')
+		{
+			inside_of_digit = 1;
+			number = (number * 10) + (s[index] - '0');
+		}
+		else if (inside_of_digit)
 		{
 			break;
 		}
-		i++;
+		index++;
 	}
 
-	return (sign * result);
+	return (sign * number);
 }
