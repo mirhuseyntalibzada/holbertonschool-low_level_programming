5cce3792fb395318a9c82a01d1bc9411077dd359
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-07-26 20:10:50 +0400

    fixed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -39,18 +39,23 @@ int main(int ac, char **av)
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", av[1]);
 
+	r = read(fd_from, buf, BUF_SIZE);
+	if (r == -1)
+		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 		error_exit(99, "Error: Can't write to %s\n", av[2]);
 
-	while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
+	while (r > 0)
 	{
 		w = write(fd_to, buf, r);
 		if (w != r)
 			error_exit(99, "Error: Can't write to %s\n", av[2]);
+		r = read(fd_from, buf, BUF_SIZE);
+		if (r == -1)
+			error_exit(98, "Error: Can't read from file %s\n", av[1]);
 	}
-	if (r == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
 
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
